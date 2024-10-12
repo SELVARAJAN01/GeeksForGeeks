6ba33e1dbb9e1497ed6453b219eb6284cf20fc3d
SELVARAJAN01 <144997943+SELVARAJAN01@users.noreply.github.com>
2024-10-12 19:29:26 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Identical Linked Lists/identical-linked-lists.cpp
+++ Difficulty: Basic/Identical Linked Lists/identical-linked-lists.cpp
@@ -64,26 +64,24 @@ class Solution {
     // Function to check if two linked lists are identical
     bool areIdentical(struct Node *head1, struct Node *head2) {
         // code here
-    
-        if(head1==NULL && head2==NULL)
-        return true;
-        if(head1==NULL || head2==NULL)
+    if(head1==NULL && head2==NULL)
+       return true;
+    if(head1==NULL || head2==NULL)
+       return false;
+    while(head1!=NULL && head2!=NULL)
+    {
+       if(head1->data!=head2->data)
         return false;
-        while(head1!=NULL && head2!=NULL)
-        {
-            if(head1->data!=head2->data)
-            return false;
-            head1=head1->next;
-            head2=head2->next;
-        }
-        if(head1==NULL && head2==NULL)
-         return true;
-        else
-         return false;
-        
-        
+       head1=head1->next;
+       head2=head2->next;
     }
-};
+
+    if(head1==NULL && head2==NULL)
+      return true;
+    else
+      return false;
+    }    
+    };
 
 //{ Driver Code Starts.
 int main() {
